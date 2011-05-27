3e7e14dd528dc571dc88f55293600aa667f767c2
Phips Peter <pspeter333@gmail.com>
2011-05-27 13:17:33 -0700

    renaming files

--- src/JsonQuery.js
+++ src/JsonQuery.js
@@ -20,18 +20,36 @@ function JsonSQL(data) {
  *
  * @this {JsonSQL}
  * @param {object} A hash of conditions
- * @return {Array} The result set for all the records that match the conditions
+ * @return {array} The result set for all the records that match the conditions
  */
 JsonSQL.prototype.where = function(conditions) {
 	
 }
 
 /**
- * Inserts an object
+ * Inserts an object into the store
  *
  * @this {JsonSQL}
- * @param {object}
+ * @param {object} The object to insert
  */
 JsonSQL.prototype.insert = function(row) {
 	this.data_.push(row);
+}
+
+/**
+ * Checks an object to see if it passes
+ * 
+ * @this {JsonSQL}
+ * @param {object} The object to check
+ * @param {object} The hash of conditions to check against
+ * @return {boolean} Returns whether or not the object passed
+ */
+JsonSQL.prototype.pass_ = function(row, conditions) {
+	// Cycle through the fields to be checked
+	for(field in conditions) {
+		// Check to see if the is a value or an object
+		
+	}
+	// Return true if gets here
+	return true;
 }
